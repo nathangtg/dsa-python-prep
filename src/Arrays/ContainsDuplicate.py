f0ddc294a64708c0b11e7d605bcdd61ee8b57cb9
nathangtg <22099865@imail.sunway.edu.my>
2025-05-28 04:36:30 +0800

    Rehearsed some old Leetcode problems

--- src/Arrays/ContainsDuplicate.py
+++ src/Arrays/ContainsDuplicate.py
@@ -1,5 +1,5 @@
 class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool :
+    def containsDuplicate(self, nums: list[int]) -> bool:
         seen = set()
 
         for num in nums:
--- src/Arrays/TopKFrequentElement.py
+++ src/Arrays/TopKFrequentElement.py
@@ -7,12 +7,13 @@ class Solution:
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
 
-        bucket = [[] for _ in range(len(nums) + 1)]
+        bucket = [[] for i in range(len(nums) - 1)]
+
+        res = []
 
         for num, count in freq.items():
             bucket[count].append(num)
 
-        res = []
         for i in range(len(bucket) - 1, 0, -1):
             for num in bucket[i]:
                 res.append(num)
