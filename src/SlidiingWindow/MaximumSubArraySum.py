29b625c9e4a5fb3803218813b9885bd8ebbe3a8a
nathangtg <22099865@imail.sunway.edu.my>
2025-05-30 10:50:46 +0800

    Grasp the concept of Array and set and more problem solving

--- src/SlidiingWindow/MaximumSubArraySum.py
+++ src/SlidiingWindow/MaximumSubArraySum.py
@@ -3,20 +3,20 @@ from typing import List
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         seen = set()
-        curr_sum = 0
-        max_sum = 0
         l = 0
+        max_sum = 0
+        current = 0
 
         for r in range(len(nums)):
             while nums[r] in seen or (r - l + 1 > k):
+                current -= nums[l]
                 seen.remove(nums[l])
-                curr_sum -= nums[l]
                 l += 1
 
             seen.add(nums[r])
-            curr_sum += nums[r]
+            current += nums[r]
 
             if r - l + 1 == k:
-                max_sum = max(curr_sum, max_sum)
+                max_sum = max(max_sum, current)
 
         return max_sum
